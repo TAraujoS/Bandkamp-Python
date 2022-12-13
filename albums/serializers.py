345f53ed995d7ac9e98cbb7f951ed730a39e96f7
Thiago AS <thiago-pin@gotmail.com>
2022-12-13 20:38:05 -0300

    feat: task 1 and 2 done, starting task 3

--- albums/serializers.py
+++ albums/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-
+from users.serializers import UserSerializer
 from .models import Album
 
 
-class AlbumSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    year = serializers.IntegerField()
-    user_id = serializers.IntegerField(read_only=True)
+class AlbumSerializer(serializers.ModelSerializer):
+    # user = UserSerializer(read_only=True)
 
-    def create(self, validated_data):
-        return Album.objects.create(**validated_data)
+    class Meta:
+        model = Album
+        fields = "__all__"
+        read_only_fields = ["user"]
--- songs/serializers.py
+++ songs/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-
+from albums.serializers import AlbumSerializer
 from .models import Song
 
 
-class SongSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    duration = serializers.CharField(max_length=255)
-    album_id = serializers.IntegerField(read_only=True)
+class SongSerializer(serializers.ModelSerializer):
+    album = AlbumSerializer(read_only=True)
 
-    def create(self, validated_data):
-        return Song.objects.create(**validated_data)
+    class Meta:
+        model = Song
+        fields = "__all__"
+        read_only_fields = ["album"]
