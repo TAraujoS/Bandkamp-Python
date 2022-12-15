f4181820cdef125f1b0ede63a27246777ea90f24
Thiago AS <thiago-pin@gotmail.com>
2022-12-15 15:24:45 -0300

    fix: deploy settings

--- bandkamp/asgi.py
+++ bandkamp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bandkamp project.
+ASGI config for bandkamp .
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bandkamp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bandkamp.settings")
 
 application = get_asgi_application()
